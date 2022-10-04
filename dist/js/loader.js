6ba2b158072e090edda38008ba0f732117f55033
leonardo <leonardo.amdev@outlook.com>
2022-10-04 13:29:56 +0200

    EDIT position loader & colors

--- dist/js/loader.js
+++ dist/js/loader.js
@@ -8,6 +8,7 @@ const fadeEffect = setInterval(() => {
     if (preloader.style.opacity > 0) {
         preloader.style.opacity -= 0.1;
     } else {
+        preloader.style.zIndex = "999";
         clearInterval(fadeEffect);
     }
 
